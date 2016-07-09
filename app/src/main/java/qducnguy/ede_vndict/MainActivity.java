5c60597048af3aed908fe777f181866342df28d2
Nguyễn Đức Quỳnh <ndquynh@tma.com.vn>
2016-07-09 14:36:51 +0700

    hiển thị thành công database lên listview

--- app/src/main/java/qducnguy/ede_vndict/MainActivity.java
+++ app/src/main/java/qducnguy/ede_vndict/MainActivity.java
@@ -1,11 +1,17 @@
 package qducnguy.ede_vndict;
 
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,17 +21,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import qducnguy.ede_vndict.database.DatabaseCreator;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, SearchView.OnQueryTextListener {
 
 
-    TextView textarea;
+    ListView listview;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +46,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,30 +64,72 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        textarea = (TextView)findViewById(R.id.tv1);  // tv is id in XML file for TextView
-        textarea.setTextSize(20);
-        textarea.setTextColor(Color.rgb(0xff, 0, 0));
-        Typeface tnFont = Typeface.createFromAsset(getAssets(), "fonts/TNKeyVN-Cgothic.ttf");
-        textarea.setTypeface(tnFont, Typeface.NORMAL);
-        createDatabase();
+
+        final ArrayList<String> database = createDatabase();
+
+        adapter = new ArrayAdapter<String>(this, R.layout.list_view_item, database)  {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                View mView = convertView ;
+                if(mView == null){
+                    LayoutInflater vi = (LayoutInflater)MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    mView = vi.inflate(R.layout.list_view_item, null);
+
+                }
+
+
+                TextView text = (TextView) mView.findViewById(R.id.list_view_item);
+                Typeface tnFont = Typeface.createFromAsset(getAssets(), "fonts/TNKeyVN-Cgothic.ttf");
+                text.setTypeface(tnFont, Typeface.NORMAL);
+
+                if(database.get(position) != null )
+                {
+                    text.setTextColor(Color.WHITE);
+                    text.setText(database.get(position));
+                    text.setBackgroundColor(Color.RED);
+                    int color = Color.argb( 200, 255, 64, 64 );
+                    text.setBackgroundColor( color );
+
+                }
+                return mView;
+
+            }
+        };
+        listview = (ListView) findViewById(R.id.lvData);
+        //set adapter cho listview
+        listview.setAdapter(adapter);
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
-    private void createDatabase() {
+    private ArrayList<String> createDatabase() {
 
         DatabaseCreator databaseCreator = new DatabaseCreator(this);
         ArrayList<String> strings = databaseCreator.readRawFile();
 
-        for(String s : strings) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                s = s.concat(System.lineSeparator());
-            } else {
-                s = s.concat("\n");
-            }
+//        for(String s : strings) {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//                s = s.concat(System.lineSeparator());
+//            } else {
+//                s = s.concat("\n");
+//            }
+//
+//            strings.add(s);
+//        }
 
-            textarea.append(s);
-        }
-
-
+        return strings;
     }
 
     @Override
@@ -87,12 +141,26 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
+    SearchView searchView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // thêm search vào vào action bar
+//        getMenuInflater().inflate(R.menu.search_view, menu);
+        MenuItem itemSearch = menu.findItem(R.id.search_view);
+        searchView = (SearchView) itemSearch.getActionView();
+        //set OnQueryTextListener cho search view để thực hiện search theo text
+        searchView.setOnQueryTextListener(this);
+        TextView searchText = (TextView)
+                searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(),"fonts/TNKeyVN-Cgothic.ttf");
+        searchText.setTypeface(myCustomFont);
         return true;
+
     }
 
     @Override
@@ -103,9 +171,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -133,5 +201,21 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)){
+            adapter.getFilter().filter("");
+            listview.clearTextFilter();
+        }else {
+            adapter.getFilter().filter(newText.toString());
+        }
+        return false;
     }
 }
